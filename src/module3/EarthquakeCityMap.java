cf5e77bf1ebf6de367eb2b808396dd70a3e3583d
gauribhoite <gauribhoite@users.noreply.github.com>
2016-06-29 08:33:09 +0100

    wip

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -27,7 +27,7 @@ import parsing.ParseFeed;
  * @author Your name here
  * Date: July 17, 2015
  * */
-public class 	EarthquakeCityMap extends PApplet {
+public class EarthquakeCityMap extends PApplet {
 
 	// You can ignore this.  It's to keep eclipse from generating a warning.
 	private static final long serialVersionUID = 1L;
@@ -72,15 +72,19 @@ public class 	EarthquakeCityMap extends PApplet {
 	    //Use provided parser to collect properties for each earthquake
 	    //PointFeatures have a getLocation method
 	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
-	    
+	    System.out.println("eathquakes.size() :"+earthquakes.size());
 	    // These print statements show you (1) all of the relevant properties 
 	    // in the features, and (2) how to get one property and use it
-	    if (earthquakes.size() > 0) {
-	    	PointFeature f = earthquakes.get(0);
-	    	System.out.println(f.getProperties());
+		for(int i=0;i<earthquakes.size();i++){
+//	    if (earthquakes.size() > 0) {
+	    	PointFeature f = earthquakes.get(i);
+//	    	System.out.println(f.getProperties());
 	    	Object magObj = f.getProperty("magnitude");
 	    	float mag = Float.parseFloat(magObj.toString());
-	    	// PointFeatures also have a getLocation method
+			// PointFeatures also have a getLocation method
+//			System.out.println("f.getLocation() :"+f.getLocation());
+			markers.add(createMarker(f));
+            System.out.println(" :"+markers.get(i).getLocation());
 	    }
 	    
 	    // Here is an example of how to use Processing's color method to generate 
@@ -88,6 +92,11 @@ public class 	EarthquakeCityMap extends PApplet {
 	    int yellow = color(255, 255, 0);
 	    
 	    //TODO: Add code here as appropriate
+        for (int i=0;i< markers.size();i++){
+            markers.get(i).setColor(yellow);
+            System.out.println(markers.get(i).);
+        }
+
 	}
 		
 	// A suggested helper method that takes in an earthquake feature and 
@@ -96,7 +105,9 @@ public class 	EarthquakeCityMap extends PApplet {
 	private SimplePointMarker createMarker(PointFeature feature)
 	{
 		// finish implementing and use this method, if it helps.
-		return new SimplePointMarker(feature.getLocation());
+        SimplePointMarker one_marker = new SimplePointMarker(feature.getLocation());
+        one_marker.setRadius(10);
+		return one_marker;
 	}
 	
 	public void draw() {
